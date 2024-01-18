996916274747c3fa3d2d28eecf10644fae710c6c
jingchen <jingchen@student.42madrid.com>
2024-01-18 20:07:42 +0100

    20240118

--- builtins/signal.c
+++ builtins/signal.c
@@ -6,30 +6,49 @@
 /*   By: jingchen <jingchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 20:41:02 by jingchen          #+#    #+#             */
-/*   Updated: 2024/01/17 21:03:16 by jingchen         ###   ########.fr       */
+/*   Updated: 2024/01/18 20:02:39 by jingchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./builtins.h"
 
-//hay que hacer sig init en el main.
+/*ctrl+c = SIGINT; ctrl+4 = SIGQUIT;
+
+- en el proceso padre; (o sea main)
 	signal(SIGINT, signal_handler);
 	signal(SIGQUIT, SIG_IGN);
-
-// en este archivo
 
 void	signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		printf("\n");
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-		g_exit_status = 1;
+		printf("\n"); -> imprimir un salto de linea;
+		rl_on_new_line(); -> inicial una linea nueva;
+		rl_replace_line("", 0); -> replace un argumento vacio
+		rl_redisplay(); -> redisplay lo de arriba;
+		g_exit_status = 1; -> salida de commando sea error, con lo cual 1.
 	}
 }
-	
+
+- y si no hay readline = NULL, (recibe un ctrl+d is igual a ENDFILE,), llama la funcion de exit y cierra el minishell.
+
+- el proceso hijo (o sea cuando gestionar heredoc o en modo bash)
+	signal(SIGINT, SIG_DEF);
+	signal(SIGQUIT, SIG_DEF);*/
+
+void	signal_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		printf ("\n"); 
+		rl_on_new_line();
+		//rl_replace_line("", 0); 
+		rl_redisplay (); 
+		g_exit_status = 1; 
+	}
+}
+
+
 /*void	sig_int(int code, t_sig g_sig)
 {
 	(void)code;
